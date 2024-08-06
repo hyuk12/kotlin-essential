0e2bfef9d03f68a389c3427c5cdab10ec8308b06
hyuk12 <hotman7025@naver.com>
2024-08-06 10:47:03 +0900

    chore: chapter3 변수의 명명,  초기화

---
+++ src/main/kotlin/chapter3/Variable.kt
@@ -0,0 +1,13 @@
+package chapter3
+
+fun main() {
+    val a = 10
+    var b = "ABC"
+    println(a) // 10
+    println(b) // ABC
+    // a = 12 <- a는 val 로 초기화 되어 읽을 수만 있기에 "Val cannot ressigned" 오류 발생
+    b = "CDE" // 값을 재할당 가능
+    println(b) // CDE
+
+}
+
